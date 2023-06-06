27fe2657389337c4f3d42f88ca7e6aa38afea693
Keruiiia <arteskeruiiia@gmail.com>
2023-06-06 17:31:24 +0300

    PYT-2958 add xcom

---
+++ dags/task_7_xcom.py
@@ -0,0 +1,97 @@
+from datetime import datetime
+import pandas as pd
+import re
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.sensors.filesystem import FileSensor
+from airflow.decorators import task_group
+from airflow.providers.mongo.hooks.mongo import MongoHook
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 5, 9),
+    'retries': 0,
+}
+
+
+def replace_null_values(**kwargs):
+    df = pd.read_csv("/home/keruiiia/airflow/data/tiktok_google_play_reviews.csv")
+    df.fillna('-', inplace=True)
+    output_file_path = "/home/keruiiia/airflow/data/tiktok_without_nulls.csv"
+    df.to_csv(output_file_path, index=False)
+    kwargs['ti'].xcom_push(key='output_file_path', value=output_file_path)
+    print(f"Saved output_file_path to XCom: {output_file_path}")
+
+
+def sort_by_created_date(**kwargs):
+    input_file_path = kwargs['ti'].xcom_pull(key='output_file_path', task_ids='replace_null_values_task')
+    print(f"Retrieved output_file_path from XCom: {input_file_path}")
+    df = pd.read_csv(input_file_path)
+    df.rename(columns={'at': 'created_date'}, inplace=True)
+    df = df.sort_values('created_date')
+    output_file_path = "/home/keruiiia/airflow/data/sorted_tiktok_without_nulls.csv"
+    df.to_csv(output_file_path, index=False)
+    kwargs['ti'].xcom_push(key='output_file_path', value=output_file_path)
+
+
+def clean_content_column(**kwargs):
+    input_file_path = kwargs['ti'].xcom_pull(key='output_file_path', task_ids='sort_by_created_date_task')
+    df = pd.read_csv(input_file_path)
+    pattern = r"[^a-zA-Z\s\.\,\?\!']"
+    df['content'] = df['content'].apply(lambda x: re.sub(pattern, '', x))
+    output_file_path = "/home/keruiiia/airflow/data/sorted_tiktok_without_nulls_and_emojis.csv"
+    df.to_csv(output_file_path, index=False)
+    kwargs['ti'].xcom_push(key='output_file_path', value=output_file_path)
+
+
+def upload_to_mongodb(**kwargs):
+    input_file_path = kwargs['ti'].xcom_pull(key='output_file_path', task_ids='clean_content_column_task')
+    mongo_conn = MongoHook(mongo_conn_id="mongo_default")
+    client = mongo_conn.get_conn()
+    db = client.task_7_database
+    collection = db.tiktok_collection
+    df = pd.read_csv(input_file_path)
+    data_dict = df.to_dict('records')
+    collection.insert_many(data_dict)
+
+
+with DAG('tiktok_processing_3',
+         default_args=default_args,
+         schedule_interval=None) as dag:
+
+    file_sensor = FileSensor(task_id='file_sensor',
+                             fs_conn_id="fs_default",
+                             filepath="tiktok_google_play_reviews.csv",
+                             poke_interval=30)
+
+    
+    @task_group()
+    def data_processing_tasks():
+        replace_null_values_task = PythonOperator(
+            task_id='replace_null_values_task',
+            python_callable=replace_null_values,
+            provide_context=True
+        )
+
+        sort_by_created_date_task = PythonOperator(
+            task_id='sort_by_created_date_task',
+            python_callable=sort_by_created_date,
+            provide_context=True
+        )
+
+        clean_content_column_task = PythonOperator(
+            task_id='clean_content_column_task',
+            python_callable=clean_content_column,
+            provide_context=True
+        )
+
+		
+    upload_to_mongodb_task = PythonOperator(
+        task_id='upload_to_mongodb_task',
+        python_callable=upload_to_mongodb,
+        provide_context=True
+    )
+
+    file_sensor >> data_processing_tasks() >> upload_to_mongodb_task
